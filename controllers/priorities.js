63ed7251102675d74d5d09800f500babbd8f4de2
Dennis Stanley <stanlsytd@gmail.com>
2014-11-17 15:12:56 -0600

    inital priority controller

--- controllers/priorities.js
+++ controllers/priorities.js
@@ -1,3 +1,16 @@
 /**
  * Created by Shruti on 11/17/14.
  */
+
+'use strict';
+
+//var Priority = require('../models/priority');
+
+exports.create = function (request, reply){
+    reply(200);
+};
+
+
+exports.show = function (request, reply){
+    reply(request.payload);
+};
